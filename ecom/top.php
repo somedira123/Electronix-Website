3e90fa314ce9be27b2606ba9baae517dcb6e2829
ShaziaSajid <70096937+ShaziaSajid@users.noreply.github.com>
2022-04-19 21:10:06 +0530

    Update top.php

--- ecom/top.php
+++ ecom/top.php
@@ -139,7 +139,7 @@ if(isset($_SESSION['USER_LOGIN'])){
                         <div class="menumenu__container clearfix">
                             <div class="col-lg-2 col-md-2 col-sm-3 col-xs-5"> 
                                 <div class="logo">
-                                     <a href="index.php"><img src="images/logo.JPG" alt="logo images"></a>
+                                     <a href="index.php"><img src="images/logo.jpg" alt="logo images"></a>
                                 </div>
                             </div>
                             <div class="col-md-7 col-lg-6 col-sm-5 col-xs-3">
